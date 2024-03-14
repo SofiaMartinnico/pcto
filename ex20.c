8121358a1effe621a502956049253cbe5b1c6bd4
SofiaMartinnico <162984844+SofiaMartinnico@users.noreply.github.com>
2024-03-14 08:42:15 +0000

    .

---
+++ ex20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int perimetro (int a, int b, int c)
+{
+    int perimetro;
+    perimetro = a + b + c;
+    return perimetro;
+}
+int main ()
+{
+    int risultato = perimetro (4, 7, 9);
+    scanf("%d %d %d", &a &b &c);
+    printf("%d", risultato);
+}
